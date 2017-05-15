7ab0cd943d0d55cd88efd6c8c2a234073c489e2e
zachwick <zach@zachwick.com>
2017-05-15 14:09:49 -0400

    WIP
    Signed-off-by: zachwick <zach@zachwick.com>

--- pages/doxygen_docs/html/jquery.js
+++ pages/doxygen_docs/html/jquery.js
@@ -1,4 +1,3 @@
-/* @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat */
 /*
  @licstart  The following is the entire license notice for the
  JavaScript code in this file.
